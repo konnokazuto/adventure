1833da715b37fc106e2d024cdf78c93c4685e92f
konnokazuto <kazu.jojo.ff@gmail.com>
2021-08-09 17:55:50 +0900

    gitを小文字大文字変更させる

---
+++ components/Organisms/Character/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="textBox__character">
+        <img v-if="imageUrl" :src="require(`@/assets/faceVariations/${imageUrl}`)" class="textBox__characterImage">
+    </div>
+</template>
+<script>
+import { mapState } from "vuex";
+
+export default {
+    computed: {
+        ...mapState({
+            plots: state => state.plot.plots,
+            plotCount: state => state.plot.plotCount,
+            plotNumber: state => state.plot.plotNumber,
+        }),
+        imageUrl () {
+            return this.plots[this.plotNumber].face
+        },
+
+    }
+}
+
+</script>
